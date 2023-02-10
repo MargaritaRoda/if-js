368409e383877ec7f726a7358d9cc298f55d6e31
MargaritaRoda <MargaritaRoda1984@gmail.com>
2023-02-10 23:49:43 +0300

    Solved task lesson-9 using Symbol.iterator.

---
+++ src/lesson-9.js
@@ -0,0 +1,37 @@
+const colors = {
+  data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
+  [Symbol.iterator]() {
+    const data = this.data;
+    return {
+      index: 0,
+
+      next() {
+        if (this.index >= data.length) {
+          this.index = 0;
+        }
+        return {
+          value: data[this.index++],
+          done: false,
+        };
+      },
+    };
+  },
+};
+
+function getChangeColorFn() {
+  console.log('create listener');
+  const iterator = colors[Symbol.iterator]();
+
+  return function changeColor(event) {
+    console.log('click');
+    event.target.style.color = iterator.next().value;
+  };
+}
+
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
+const text3 = document.getElementById('text3');
+
+text1.addEventListener('click', getChangeColorFn());
+text2.addEventListener('click', getChangeColorFn());
+text3.addEventListener('click', getChangeColorFn());
