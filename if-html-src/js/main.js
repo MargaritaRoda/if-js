10cf5bca6212bf6f746edac377cfe4e1d7bf4729
MargaritaRoda <MargaritaRoda1984@gmail.com>
2023-02-16 19:02:32 +0300

    Added form for details of number of adults, children and rooms.

--- if-html-src/js/main.js
+++ if-html-src/js/main.js
@@ -78,3 +78,185 @@ function addHomesItems(containerElement, homesItemsData) {
 }
 
 addHomesItems(document.querySelector('.homes__items'), homesItemsData);
+
+document
+  .querySelector('.top-section__input--adult')
+  .addEventListener('click', showAdultsForm);
+document
+  .querySelector('.top-section__input--child')
+  .addEventListener('click', showAdultsForm);
+document
+  .querySelector('.top-section__input--room')
+  .addEventListener('click', showAdultsForm);
+
+function showAdultsForm() {
+  const list = document.querySelector('.adults-form');
+  list.classList.toggle('adults-form--hidden');
+}
+
+function makeMinusListener(
+  numberSelector,
+  plusBtnSelector,
+  min,
+  max,
+  inputSelector,
+) {
+  const number = document.querySelector(numberSelector);
+  const input = document.querySelector(inputSelector);
+  const plusBtn = document.querySelector(plusBtnSelector);
+  return function (event) {
+    event.preventDefault();
+    let value = parseInt(number.textContent, 10);
+
+    if (value === max) {
+      plusBtn.removeAttribute('disabled');
+    }
+
+    value -= 1;
+    number.textContent = value;
+    if (value === min) {
+      event.target.setAttribute('disabled', '');
+    }
+
+    input.value = `${value} ${input.dataset.prefix}`;
+  };
+}
+function makePlusListener(
+  numberSelector,
+  minusBtnSelector,
+  min,
+  max,
+  inputSelector,
+) {
+  const number = document.querySelector(numberSelector);
+  const input = document.querySelector(inputSelector);
+  const minusBtn = document.querySelector(minusBtnSelector);
+  return function (event) {
+    event.preventDefault();
+    let value = parseInt(number.textContent, 10);
+    if (value === min) {
+      minusBtn.removeAttribute('disabled');
+    }
+    value += 1;
+    number.textContent = value;
+    if (value === max) {
+      event.target.setAttribute('disabled', '');
+    }
+
+    input.value = `${value} ${input.dataset.prefix}`;
+  };
+}
+
+document
+  .querySelector('.adults-btn--minus')
+  .addEventListener(
+    'click',
+    makeMinusListener(
+      '.adults-form__number-adult',
+      '.adults-btn--plus',
+      1,
+      30,
+      '.top-section__input--adult',
+    ),
+  );
+document
+  .querySelector('.adults-btn--plus')
+  .addEventListener(
+    'click',
+    makePlusListener(
+      '.adults-form__number-adult',
+      '.adults-btn--minus',
+      1,
+      30,
+      '.top-section__input--adult',
+    ),
+  );
+document
+  .querySelector('.children-btn--minus')
+  .addEventListener(
+    'click',
+    makeMinusListener(
+      '.adults-form__number-children',
+      '.children-btn--plus',
+      0,
+      10,
+      '.top-section__input--child',
+    ),
+  );
+document
+  .querySelector('.children-btn--plus')
+  .addEventListener(
+    'click',
+    makePlusListener(
+      '.adults-form__number-children',
+      '.children-btn--minus',
+      1,
+      10,
+      '.top-section__input--child',
+    ),
+  );
+document
+  .querySelector('.room-btn--minus')
+  .addEventListener(
+    'click',
+    makeMinusListener(
+      '.adults-form__number-room',
+      '.room-btn--plus',
+      1,
+      30,
+      '.top-section__input--room',
+    ),
+  );
+document
+  .querySelector('.room-btn--plus')
+  .addEventListener(
+    'click',
+    makePlusListener(
+      '.adults-form__number-room',
+      '.room-btn--minus',
+      1,
+      30,
+      '.top-section__input--room',
+    ),
+  );
+
+document
+  .querySelector('.children-btn--plus')
+  .addEventListener('click', handleChildrenButtonsClick);
+document
+  .querySelector('.children-btn--minus')
+  .addEventListener('click', handleChildrenButtonsClick);
+function handleChildrenButtonsClick() {
+  const numberOfChildren = document.querySelector(
+    '.adults-form__number-children',
+  );
+  const value = parseInt(numberOfChildren.textContent, 10);
+  createChildrenList(value);
+}
+
+function createChildrenList(value) {
+  const adultsFormChildren = document.querySelector('.adults-form__children');
+  if (value !== 0) {
+    adultsFormChildren.classList.remove('adults-form__children--hidden');
+  } else {
+    adultsFormChildren.classList.add('adults-form__children--hidden');
+  }
+
+  const childContainer = document.querySelector(
+    '.adults-form__children-container',
+  );
+  childContainer.innerHTML = '';
+  for (let i = 1; i <= value; i++) {
+    const select = document.createElement('select');
+    select.setAttribute('class', 'adults-form__child-age');
+
+    const str = 'years old';
+    let strOption = '';
+
+    for (let j = 0; j <= 17; j++) {
+      strOption += `<option value="${j}">${j} ${str}</option>`;
+    }
+    select.innerHTML = strOption;
+    childContainer.appendChild(select);
+  }
+}
