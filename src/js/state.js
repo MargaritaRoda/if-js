7c35a1206d70e21a1c9ba4e597d58fd74435791d
MargaritaRoda <MargaritaRoda1984@gmail.com>
2023-03-16 21:59:09 +0300

    Added modules of code according function

--- src/js/state.js
+++ src/js/state.js
@@ -8,6 +8,7 @@ const state = {
 
   set(name, value) {
     this[name] = value;
+    console.log(this);
     this.handleStateUpdate(name);
   },
 
---
+++ src/js/views/homesItems.js
@@ -0,0 +1,88 @@
+import { fetchHotels } from '../api.js';
+import state from '../state.js';
+import { bubbleSort } from '../utils.js';
+import { POPULAR_HOTELS_KEY } from '../config.js';
+
+function addHomesItems(blockElement, homesItemsData) {
+  const containerElement = blockElement.querySelector('.homes__items');
+  const nextBtn = blockElement.querySelector('.homes__slider-next');
+  nextBtn.style.display = homesItemsData.length <= 4 ? 'none' : 'flex';
+
+  containerElement.innerHTML = '';
+  homesItemsData.forEach((item) => {
+    const article = document.createElement('article');
+    article.innerHTML += `<article class = 'homes__item'>
+        <img src="${item.imageUrl}" alt="${item.name}" class="homes__item-img">
+        <div class='homes__item-title'>${item.name} </div>
+        <div class='homes__item-address'>${item.city} ${item.country}</div>
+      </article>`;
+    containerElement.appendChild(article);
+  });
+}
+
+async function getHomesItemsData() {
+  const response = await fetchHotels({});
+  const homesItemsData = await response.json();
+  const sortedHomesItemsData = bubbleSort(homesItemsData, (item1, item2) => {
+    const currentName = item1['name'];
+    const nextName = item2['name'];
+    return currentName.localeCompare(nextName);
+  });
+  sessionStorage.setItem(
+    POPULAR_HOTELS_KEY,
+    JSON.stringify(sortedHomesItemsData),
+  );
+  return sortedHomesItemsData;
+}
+async function renderPopularHotels() {
+  const blockEl = document.querySelector('#js-popular-hotels');
+
+  if (!(POPULAR_HOTELS_KEY in sessionStorage)) {
+    try {
+      addHomesItems(blockEl, await getHomesItemsData());
+    } catch (err) {
+      console.log('Fetch Error :-S', err);
+    }
+  } else {
+    let homesItemsData;
+    try {
+      homesItemsData = JSON.parse(sessionStorage.getItem(POPULAR_HOTELS_KEY));
+    } catch (err) {
+      try {
+        homesItemsData = await getHomesItemsData();
+      } catch (err) {
+        console.log('Fetch Error :-S', err);
+      }
+    }
+
+    addHomesItems(blockEl, homesItemsData);
+  }
+}
+
+async function render() {
+  const inputPlace = document.querySelector('.top-section__input--place');
+  const btn = document.querySelector('.top-section__submit-btn');
+  btn.addEventListener('click', async () => {
+    try {
+      const response = await fetchHotels({
+        search: inputPlace.value.trim(),
+        adults: state.get('adults'),
+        rooms: state.get('rooms'),
+        childrenAges: state.get('childrenAges'),
+        // .filter((age) => Number.isInteger(age))
+      });
+      const homesItemsData = await response.json();
+      addHomesItems(
+        document.getElementById('js-available_hotels'),
+        homesItemsData,
+      );
+      document.querySelector('.homes').removeAttribute('hidden');
+    } catch (err) {
+      console.log('Fetch Error :-S', err);
+      document.querySelector('.homes').setAttribute('hidden', '');
+    }
+  });
+  await renderPopularHotels();
+}
+
+export default render;
